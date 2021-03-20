94373de6184168d597184895f2af95f6a4d4d9ca
Victor Manduca <victor.manduca@helpper.com.br>
2021-03-20 20:26:27 -0300

    feature: create user with encrypted password and list all users

--- server-node/src/Controllers/user.controller.ts
+++ server-node/src/Controllers/user.controller.ts
@@ -1,9 +1,32 @@
 import { Request, Response } from 'express'
 
-import { ok } from '../Shared/response.shared'
+import { encryptPassword } from '../Shared/crypto.shared'
+import { created, ok, badRequest } from '../Shared/response.shared'
+import { User } from '../models/entities/User'
 
-export default class User {
-	public index(req: Request, res: Response) {
-		return ok(res, { nome: 'Victor', password: 'pass' })
+export default class UserController {
+	public async index(req: Request, res: Response) {
+		try {
+			const allUsers = await User.find()
+			return ok(res, allUsers)
+		} catch (error) {
+			return badRequest(res, error)
+		}
+	}
+
+	public async create(req: Request, res: Response) {
+		try {
+			const body = req.body
+			const user: User = new User
+
+			user.email = body.email
+			user.name = body.name
+			user.password =  encryptPassword(body.password)
+			await user.save()
+
+			return created(res)
+		} catch (error) {
+			return badRequest(res, error)
+		}
 	}
 }
