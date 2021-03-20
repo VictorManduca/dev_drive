309129608461c423d263a7692fa05352ebc76848
Victor Manduca <victor.manduca@helpper.com.br>
2021-03-20 20:29:59 -0300

    feature: implement method to encrypt password

---
+++ server-node/src/Shared/crypto.shared.ts
@@ -0,0 +1,7 @@
+import crypto from 'crypto'
+
+const secreat: string = '123456abc'
+
+export function encryptPassword(password: string): string {
+  return crypto.createHmac('sha256', secreat).update(password).digest('hex')
+}
