616731c0c54b056754049afe9145468e7a4e69b8
Victor Manduca <victor.manduca@helpper.com.br>
2021-03-21 01:03:55 -0300

    feature: implement login and jwt middleware

--- server-node/src/routes.ts
+++ server-node/src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 
+import AuthMiddleware from './Middlewares/auth.middleware'
+
 import UserController from './Controllers/user.controller'
+
+const authMiddleware: AuthMiddleware = new AuthMiddleware()
 
 const userController: UserController = new UserController
 
 const routes = Router()
 
-routes.get('/user', userController.index)
-routes.post('/user', userController.create)
+routes.post('/user/login', userController.login)
+routes.get('/user', authMiddleware.verifyJwt, userController.index)
+routes.post('/user', authMiddleware.verifyJwt, userController.create)
 
 export default routes
