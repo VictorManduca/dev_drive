89f7e35c063793af8b51ce657b8a59d21650b55a
Victor Manduca <victor.manduca@helpper.com.br>
2021-03-21 01:05:35 -0300

    feature: encrypt password

--- server-node/src/Shared/crypto.shared.ts
+++ server-node/src/Shared/crypto.shared.ts
@@ -1,7 +1,10 @@
 import crypto from 'crypto'
+import dotenv from 'dotenv'
+import path from 'path'
 
-const secreat: string = '123456abc'
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
+const secret: string = `${process.env.SECRET}`
 export function encryptPassword(password: string): string {
-	return crypto.createHmac('sha256', secreat).update(password).digest('hex')
+	return crypto.createHmac('sha256', secret).update(password).digest('hex')
 }
